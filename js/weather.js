e6cf6e743abc798f7825dc3edcb040aba91f7a17
Woori Kang <rose5307@naver.com>
2021-07-12 03:13:45 +0900

    add weather icon

--- js/weather.js
+++ js/weather.js
@@ -3,18 +3,20 @@ const API_KEY = "e624cfe13d5384545872f83e1779948d";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=kr&appid=${API_KEY}&units=metric
     `;
     fetch(url).then((response) => response.json()).then((data) =>{
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
+        const weather = document.querySelector("#weather .weather");
+        const icon = document.querySelector("#weather .icon");
+        const weatherIcon = data.weather[0].icon;
+        icon.setAttribute('src', `http://openweathermap.org/img/w/${weatherIcon}.png`);
+        weather.innerText = `${data.name} / ${data.main.temp}℃`;
     });
 
 }
 function onGeoError(){
-
+    const weather = document.querySelector("#weather .weather");
+    weather.innerText = "undefined. please allow location.";
 }
 
 
